b8367f7abd450fcc11e0703f2c9533d8f6f82438
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-08-15 08:30:24 -0600

    creacion

---
+++ src/ahorcadoprogra2lab_samuel_leonardo/AhorcadoProgra2Lab_Samuel_Leonardo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ahorcadoprogra2lab_samuel_leonardo;
+
+/**
+ *
+ * @author unwir
+ */
+public class AhorcadoProgra2Lab_Samuel_Leonardo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
