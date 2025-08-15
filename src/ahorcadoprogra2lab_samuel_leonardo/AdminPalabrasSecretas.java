aa1c6fa00b18027663484bf3545d7b5048ee1f23
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-08-15 09:06:02 -0600

    logica inicio

--- src/ahorcadoprogra2lab_samuel_leonardo/AdminPalabrasSecretas.java
+++ src/ahorcadoprogra2lab_samuel_leonardo/AdminPalabrasSecretas.java
@@ -4,12 +4,17 @@
  */
 package ahorcadoprogra2lab_samuel_leonardo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  *
  * @author unwir
  */
 public class AdminPalabrasSecretas {
     private static AdminPalabrasSecretas instance;
+    private ArrayList<String> palabrasSecretas = new ArrayList<>();
+    
     
     public static AdminPalabrasSecretas getInstance(){
         if(instance == null){
@@ -17,5 +22,13 @@ public class AdminPalabrasSecretas {
         }
         return instance;
     }
+    
+    
+    public void addPrimerasPalabras(){
+        String[] palbarras = {"Leon", "Secreto", "Mago", "Filtro", "Proyecto", 
+            "Cable", "Puntos", "Deidad",  "Artificial", "Loco"};
+        palabrasSecretas.addAll(Arrays.asList(palbarras));
+    }
+   
     
 }
